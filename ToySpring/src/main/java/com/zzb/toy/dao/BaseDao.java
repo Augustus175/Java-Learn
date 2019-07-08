96dd67dac0f045430e26d5a4b1145d60f3e1ec5d
gavin <columbia7311@163.com>
2019-07-08 22:30:44 +0800

    2019-07-08 22:29:57

---
+++ ToySpring/src/main/java/com/zzb/toy/dao/BaseDao.java
@@ -0,0 +1,7 @@
+package com.zzb.toy.dao;
+
+public interface BaseDao {
+    void save();
+
+    void delete();
+}
--- ToySpring/src/main/java/com/zzb/toy/service/EmpService.java
+++ ToySpring/src/main/java/com/zzb/toy/service/EmpService.java
@@ -1,6 +1,10 @@
 package com.zzb.toy.service;
 
+import com.zzb.toy.dao.BaseDao;
+
 public class EmpService extends ContextAction implements BaseService {
+    private BaseDao dao;
+
     public String save() {
         System.out.println("-------run EmpService save-------");
         return "success.jsp";
